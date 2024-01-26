ed75e87c16baa674987cc313de62f2abe61188fd
Joel Hagan <joel.hagan.8@gmail.com>
2024-01-26 14:27:32 -0700

    implement tech component for ball models

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+
+import { BallCanvas } from "./canvas";
+import { SectionWrapper } from "./hoc";
+import { technologies } from "../constants";
+import { motion } from "framer-motion";
+import { textVariant } from "../utils/motion";
+import { styles } from "../styles";
 
 const Tech = () => {
   return (
-    <div>Tech</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText} text-center`}>Tech Stack</p>
+        <h2 className={`${styles.sectionHeadText} text-center mb-16`}>
+          Things I Like To Use.
+        </h2>
+      </motion.div>
 
-export default Tech
+      <div className="flex flex-row flex-wrap justify-center gap-10">
+        {technologies.map((technology) => (
+          <div
+            className="w-28 h-28"
+            key={technology.name}
+          >
+            <BallCanvas icon={technology.icon} />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(Tech, "");
